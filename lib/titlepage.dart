b0acee433ededb6e2f34c402dc1d936dc8744254
Justin Mak <jp.mak44@gmail.com>
2023-04-28 13:30:59 -0400

    Wrapped buttons and added padding for cleanliness

--- lib/titlepage.dart
+++ lib/titlepage.dart
@@ -69,7 +69,7 @@ class _ButtonNavigationState extends State<ButtonNavigation> {
                   ),
                   SizedBox(height: 30.0),
                   Text(
-                    "Instantly translate text from images with our AI-powered app. Perfect for translating foreign notes, transcribing documents, and capturing text from signs and monuments.",
+                    "Instantly translate text from images with our AI-powered app. Perfect for translating foreign notes and capturing text from signs and monuments.",
                     style: TextStyle(
                       fontSize: 18.0,
                       height: 1.5,
@@ -78,86 +78,98 @@ class _ButtonNavigationState extends State<ButtonNavigation> {
                       color: Colors.grey,
                     ),
                   ),
+                ], // End of text column
+              ),
+            ),
+          ),
+          Container(
+            width: 350,
+            padding: EdgeInsets.only(bottom: 30),
+            child: Align(
+              alignment: Alignment.center,
+              child: Column(
+                children: [
+                  SizedBox(
+                    width: 270.0,
+                    height: 86.0,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        primary: Color.fromARGB(224, 104, 164, 255),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25.0),
+                        ),
+                      ),
+                      child: Container(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              // Will update; use 'const' literals as arguments to constructors of '@immutable' classes.
+                              'Start Translating',
+                              textAlign: TextAlign.right,
+                              style: TextStyle(
+                                fontFamily: 'Times New Roman',
+                                fontStyle: FontStyle.normal,
+                                fontWeight: FontWeight.w700,
+                                fontSize: 20.0,
+                                color: Color.fromARGB(255, 255, 255, 255),
+                              ),
+                            ),
+                            Icon(
+                              Icons.arrow_forward_ios,
+                              color: Colors.white,
+                              size: 20.0,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                      height: 30.0), // Add some spacing between the buttons
+                  SizedBox(
+                    width: 270.0,
+                    height: 83.0,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        primary: Color.fromARGB(223, 255, 219, 152),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25.0),
+                        ),
+                      ),
+                      child: Container(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'Pick a New Language',
+                              textAlign: TextAlign.right,
+                              style: TextStyle(
+                                fontFamily: 'Times New Roman',
+                                fontStyle: FontStyle.normal,
+                                fontWeight: FontWeight.w700,
+                                fontSize: 20.0,
+                                color: Color.fromARGB(255, 255, 255, 255),
+                              ),
+                            ),
+                            Icon(
+                              Icons.arrow_forward_ios,
+                              color: Colors.white,
+                              size: 20.0,
+                            ),
+                          ],
+                        ),
+                      ),
+                      // End of second button
+                    ),
+                  ),
                 ],
               ),
             ),
           ),
-
-          SizedBox(
-            width: 330.0,
-            height: 86.0,
-            child: ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                primary: Color.fromARGB(224, 104, 164, 255),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(25.0),
-                ),
-              ),
-              child: Container(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      // Will update; use 'const' literals as arguments to constructors of '@immutable' classes.
-                      'Start Translating',
-                      textAlign: TextAlign.right,
-                      style: TextStyle(
-                        fontFamily: 'Times New Roman',
-                        fontStyle: FontStyle.normal,
-                        fontWeight: FontWeight.w700,
-                        fontSize: 20.0,
-                        color: Color.fromARGB(255, 255, 255, 255),
-                      ),
-                    ),
-                    Icon(
-                      Icons.arrow_forward_ios,
-                      color: Colors.white,
-                      size: 20.0,
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-          SizedBox(height: 30.0), // Add some spacing between the buttons
-          SizedBox(
-            width: 330.0,
-            height: 83.0,
-            child: ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                primary: Color.fromARGB(223, 255, 219, 152),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(25.0),
-                ),
-              ),
-              child: Container(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      'Pick a New Language',
-                      textAlign: TextAlign.right,
-                      style: TextStyle(
-                        fontFamily: 'Times New Roman',
-                        fontStyle: FontStyle.normal,
-                        fontWeight: FontWeight.w700,
-                        fontSize: 20.0,
-                        color: Color.fromARGB(255, 255, 255, 255),
-                      ),
-                    ),
-                    Icon(
-                      Icons.arrow_forward_ios,
-                      color: Colors.white,
-                      size: 20.0,
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        ],
+        ], // End of children container
       ),
       body: SizedBox(
         height: double.infinity,
