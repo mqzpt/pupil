6ab340f8410a8d88bffa6ab61dea103fa61655c9
Justin Mak <jp.mak44@gmail.com>
2023-04-28 13:45:57 -0400

    Centred the text and buttons with respect to the screen

--- lib/titlepage.dart
+++ lib/titlepage.dart
@@ -41,128 +41,143 @@ class _ButtonNavigationState extends State<ButtonNavigation> {
       floatingActionButton: Column(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
-          SizedBox(
-            width: 270,
-            height: 400,
-            child: Align(
-              alignment: Alignment.centerLeft,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Image to Text Translator',
-                    style: TextStyle(
-                      fontSize: 40.0,
-                      fontFamily: 'Garamond',
-                      fontStyle: FontStyle.normal,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Text(
-                    'AI Camera App',
-                    style: TextStyle(
-                      fontSize: 20.0,
-                      fontFamily: 'Roboto',
-                      fontStyle: FontStyle.normal,
-                      color: Colors.grey,
-                    ),
-                  ),
-                  SizedBox(height: 30.0),
-                  Text(
-                    "Instantly translate text from images with our AI-powered app. Perfect for translating foreign notes and capturing text from signs and monuments.",
-                    style: TextStyle(
-                      fontSize: 18.0,
-                      height: 1.5,
-                      fontFamily: 'Roboto',
-                      fontStyle: FontStyle.normal,
-                      color: Colors.grey,
-                    ),
-                  ),
-                ], // End of text column
-              ),
-            ),
-          ),
           Container(
-            width: 350,
-            padding: EdgeInsets.only(bottom: 30),
+            padding: EdgeInsets.only(left: 25),
             child: Align(
               alignment: Alignment.center,
               child: Column(
                 children: [
                   SizedBox(
-                    width: 270.0,
-                    height: 86.0,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        primary: Color.fromARGB(224, 104, 164, 255),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(25.0),
-                        ),
-                      ),
-                      child: Container(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              // Will update; use 'const' literals as arguments to constructors of '@immutable' classes.
-                              'Start Translating',
-                              textAlign: TextAlign.right,
-                              style: TextStyle(
-                                fontFamily: 'Times New Roman',
-                                fontStyle: FontStyle.normal,
-                                fontWeight: FontWeight.w700,
-                                fontSize: 20.0,
-                                color: Color.fromARGB(255, 255, 255, 255),
-                              ),
+                    width: 270,
+                    height: 400,
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Image to Text Translator',
+                            style: TextStyle(
+                              fontSize: 40.0,
+                              fontFamily: 'Garamond',
+                              fontStyle: FontStyle.normal,
+                              fontWeight: FontWeight.bold,
                             ),
-                            Icon(
-                              Icons.arrow_forward_ios,
-                              color: Colors.white,
-                              size: 20.0,
+                          ),
+                          Text(
+                            'AI Camera App',
+                            style: TextStyle(
+                              fontSize: 20.0,
+                              fontFamily: 'Roboto',
+                              fontStyle: FontStyle.normal,
+                              color: Colors.grey,
                             ),
-                          ],
-                        ),
+                          ),
+                          SizedBox(height: 30.0),
+                          Text(
+                            "Instantly translate text from images with our AI-powered app. Perfect for translating foreign notes and capturing text from signs and monuments.",
+                            style: TextStyle(
+                              fontSize: 18.0,
+                              height: 1.5,
+                              fontFamily: 'Roboto',
+                              fontStyle: FontStyle.normal,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ], // End of text column
                       ),
                     ),
                   ),
-                  SizedBox(
-                      height: 30.0), // Add some spacing between the buttons
-                  SizedBox(
-                    width: 270.0,
-                    height: 83.0,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        primary: Color.fromARGB(223, 255, 219, 152),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(25.0),
-                        ),
-                      ),
-                      child: Container(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              'Pick a New Language',
-                              textAlign: TextAlign.right,
-                              style: TextStyle(
-                                fontFamily: 'Times New Roman',
-                                fontStyle: FontStyle.normal,
-                                fontWeight: FontWeight.w700,
-                                fontSize: 20.0,
-                                color: Color.fromARGB(255, 255, 255, 255),
+                  Container(
+                    width: 350,
+                    padding: EdgeInsets.only(bottom: 30),
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            width: 270.0,
+                            height: 86.0,
+                            child: ElevatedButton(
+                              onPressed: () {},
+                              style: ElevatedButton.styleFrom(
+                                primary: Color.fromARGB(224, 104, 164, 255),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(25.0),
+                                ),
+                              ),
+                              child: Container(
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      // Will update; use 'const' literals as arguments to constructors of '@immutable' classes.
+                                      'Start Translating',
+                                      textAlign: TextAlign.right,
+                                      style: TextStyle(
+                                        fontFamily: 'Times New Roman',
+                                        fontStyle: FontStyle.normal,
+                                        fontWeight: FontWeight.w700,
+                                        fontSize: 20.0,
+                                        color:
+                                            Color.fromARGB(255, 255, 255, 255),
+                                      ),
+                                    ),
+                                    Icon(
+                                      Icons.arrow_forward_ios,
+                                      color: Colors.white,
+                                      size: 20.0,
+                                    ),
+                                  ],
+                                ),
                               ),
                             ),
-                            Icon(
-                              Icons.arrow_forward_ios,
-                              color: Colors.white,
-                              size: 20.0,
+                          ),
+                          SizedBox(
+                              height:
+                                  30.0), // Add some spacing between the buttons
+                          SizedBox(
+                            width: 270.0,
+                            height: 83.0,
+                            child: ElevatedButton(
+                              onPressed: () {},
+                              style: ElevatedButton.styleFrom(
+                                primary: Color.fromARGB(223, 255, 219, 152),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(25.0),
+                                ),
+                              ),
+                              child: Container(
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      'Pick a New Language',
+                                      textAlign: TextAlign.right,
+                                      style: TextStyle(
+                                        fontFamily: 'Times New Roman',
+                                        fontStyle: FontStyle.normal,
+                                        fontWeight: FontWeight.w700,
+                                        fontSize: 20.0,
+                                        color:
+                                            Color.fromARGB(255, 255, 255, 255),
+                                      ),
+                                    ),
+                                    Icon(
+                                      Icons.arrow_forward_ios,
+                                      color: Colors.white,
+                                      size: 20.0,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              // End of second button
                             ),
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
-                      // End of second button
                     ),
                   ),
                 ],
