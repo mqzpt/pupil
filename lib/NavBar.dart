b75be654a1d11f49f75b3af55a9b8e154297a772
Matthew Athanasopoulos <matthew.athanasopoulos@gmail.com>
2023-04-30 14:01:41 -0400

    Change navbar file name to match snakecase

--- lib/NavBar.dart
+++ lib/NavBar.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+// import 'package:share_plus/share_plus.dart';
+
+String username = "Matthew";
+String useremail = "mk2athan@uwaterloo.ca";
 
 class NavBar extends StatelessWidget {
   @override
@@ -7,8 +11,8 @@ class NavBar extends StatelessWidget {
     return Drawer(
         child: ListView(padding: EdgeInsets.zero, children: [
       UserAccountsDrawerHeader(
-          accountName: Text('JP Mak'),
-          accountEmail: Text('j29mak@uwaterloo.ca'),
+          accountName: Text(username),
+          accountEmail: Text(useremail),
           currentAccountPicture: CircleAvatar(
               child: ClipOval(
                   child: Image.asset(
@@ -17,20 +21,42 @@ class NavBar extends StatelessWidget {
             height: 90,
             fit: BoxFit.cover,
           ))),
-          decoration: BoxDecoration(
+          decoration: const BoxDecoration(
               color: Colors.blue,
               image: DecorationImage(
                 image: AssetImage('assets/icon/icon.png'),
                 fit: BoxFit.cover,
               ))),
+      // ListTile(
+      //   leading: const Icon(Icons.share),
+      //   title: const Text('Profile'),
+      //   onTap: () async {
+      //     // Show pop-up menu and wait for user selection
+      //     final result = await showMenu(
+      //       context: context,
+      //       position: RelativeRect.fromLTRB(0, 0, 0, 0),
+      //       items: [
+      //         PopupMenuItem(
+      //           child: Text('Share via iOS Share Sheet'),
+      //           value: 'share',
+      //         ),
+      //       ],
+      //     );
+
+      //     // Handle user selection
+      //     if (result == 'share') {
+      //       await Share.share('https://github.com/mqzpt');
+      //     }
+      //   },
+      // ),
       ListTile(
-        leading: Icon(Icons.share),
-        title: Text('Share'),
+        leading: const Icon(Icons.star),
+        title: const Text('Certificates'),
         onTap: () => null,
       ),
       ListTile(
-        leading: Icon(Icons.star),
-        title: Text('Certificates'),
+        leading: const Icon(Icons.share),
+        title: const Text('Profile'),
         onTap: () => null,
       ),
     ]));
